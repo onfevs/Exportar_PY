dd37b0d0d80fadbbc15df2a685008838b43c8812
OnfeVS <jorgevallejo2990@gmail.com>
2023-06-10 20:49:21 -0500

    Update Membrete.py

--- Membrete.py
+++ Membrete.py
@@ -11,11 +11,6 @@ mapas = [mapa for mapa in os.listdir(carpeta) if mapa.endswith(".mxd")]
 # Resolución en puntos por pulgada (dpi)
 resolucion = 300
 
-#Esto se debe mejorar o quitar
-# Definir el mensaje que se quiere agregar después de los dos puntos
-observacion = "Pendejo"
-
-#Esto se debe quitar exportados para que no cree otra carpeta
 # Carpeta de destino para los archivos PDF y JPEG
 carpeta_destino = r"C:\Users\Onfe\Desktop\3X\Exportados"
 
@@ -40,11 +35,9 @@ for i, mapa in enumerate(mapas):
         if elemento.type == "TEXT_ELEMENT" and "<BOL>" in elemento.text:
             # Cambiar el texto del cuadro de texto con el número del mapa y el número total de mapas
             elemento.text = "<BOL> Mapa </BOL> {} <BOL> de </BOL> {}".format(i+1, len(mapas))
-            #Esto se debe mejorar o quitar
         # Verificar si el elemento es un cuadro de texto y si su texto empieza con "Observaciones:"
-        if elemento.type == "TEXT_ELEMENT" and elemento.text.startswith("Observaciones:"):
-            # Agregar el mensaje al texto del cuadro de texto después de los dos puntos
-            elemento.text = elemento.text + " " + observacion
+        elif elemento.name == "Observaciones":
+            elemento.text = "Nuevas observaciones"
     
     # Guardar los cambios en el archivo .mxd original
     mxd.save()
@@ -70,6 +63,5 @@ for i, mapa in enumerate(mapas):
 
 # Imprimir un mensaje de finalización
 print("Se han exportado todos los mapas a pdf y jpg con la calidad mejorada")
-
 
 
